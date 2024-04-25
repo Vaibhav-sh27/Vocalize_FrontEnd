b9f5c43a4c344178046f8aede45776e7a2001565
Vaibhav-sh27 <Vaibhavshrotriyas@gmail.com>
2024-04-25 12:31:06 +0530

    date fix

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -4,12 +4,13 @@ import { v4 as uuidv4 } from 'uuid';
 import { Context } from '../contexts/Context';
 import axios from "axios";
 import { useAuth } from "../contexts/AuthContext";
+
 function NewTask() {
   let [inp, setInp] = useState('');
   let dateRef = useRef();
   const {array, setarr} = useContext(Context);
   const {currUser} = useAuth();
-
+  let dd= new Date().toISOString().slice(0,16); 
   
   async function add(item) {
     if(inp){
@@ -21,18 +22,21 @@ function NewTask() {
       setarr((item) => [...item, res.data.data]);
       setInp("");
     }
-    
   }
   return (
     <div className={styles.form}>
       <div className={styles.row}>
         <label htmlFor="cityName">New Task</label>
         <input id="cityName" placeholder="Create a new Task" onChange={(e)=>{setInp(e.target.value)}} value={inp} />
-        <input id="Time" placeholder="Time" type="datetime-local" ref={dateRef} />
+        <input id="Time" placeholder="Time" name="date-time" type="datetime-local" min={dd}  ref={dateRef}  />
         <button  className={styles.btn} onClick={()=>{add(inp)}}>ADD </button>
       </div>
     </div>
   );
 }
+
+
+
+
 
 export default NewTask;
